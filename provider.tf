48e9997982fd4d0af4b4f3451e978f0f0696396b
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-18 12:14:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -26,6 +26,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+  alias                         = "app-sub"
+  subscription_id = "2bd94ec0-b962-4bad-84a8-7900bb20b520"
+  tenant_id              = "73d3d213-f87a-4465-9a7e-67bd625fdf9c"
+  client_id                = "b57b2822-9a42-44c6-b239-d37ff4a8e782"
+  client_secret      = "Q3w8Q~kFXyWJDQFzq6CnoYJNfrTUBE5RmnZR_ai_"
+  }
 }
 
