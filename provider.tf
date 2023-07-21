49e337c6a8ad7f3dc27a9766d781ee87a00ff08e
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-21 15:12:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,16 +19,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
+      version = "=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  client_id       = "326896e5-6eaf-4631-b3b0-1dcf196f489f"
+  client_secret   = "9f78Q~7_dTDrFXUEln97CjLcbG.Ue.wIQzTcEbiA"
+  tenant_id       = "73d3d213-f87a-4465-9a7e-67bd625fdf9c"
+  subscription_id = "2bd94ec0-b962-4bad-84a8-7900bb20b520"
 }
 
 
