52ebdfb390e83de82064b52293ead1e9c0ac38ad
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-21 14:50:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,20 +13,6 @@
 #  }
 # }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
-    }
-  }
-}
 
-provider "azurerm" {
-  features {}
-}
 
 
