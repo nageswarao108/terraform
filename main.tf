b0a6d0698ebc7fbc90d55a39d3d4753073b81c0d
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-26 16:13:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_share" "FSShare" {
-  name                 = "fslogix"
+  name                 = "${fslogix}"
   storage_account_name = azurerm_storage_account.storage.name
   depends_on           = [azurerm_storage_account.storage]
 }
