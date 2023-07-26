ea89b84bafe8ae47213d8a46858fa928f72b0051
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-26 18:04:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_share" "FSShare" {
-  name                 = param.name
+  name                 = $(name)
   storage_account_name = azurerm_storage_account.storage.name
   depends_on           = [azurerm_storage_account.storage]
 }
