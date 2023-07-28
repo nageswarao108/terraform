9c4ef11ee501f1f63836324622756ff5dcbda738
nageswarao128 <73346292+nageswarao128@users.noreply.github.com>
2023-07-28 11:56:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,26 +21,38 @@ resource "random_string" "random" {
 }
 
 
-resource "azurerm_service_plan" "appserviceplan" {
+resource "azurerm_app_service_plan" "example" {
   name                = var.appplan_name
   location            = "east us"
   resource_group_name = "NageswarRG"
-  os_type             = "Windows"
-  sku_name            = "B1"
-}
 
-
-resource "azurerm_linux_web_app" "webapp" {
-  name                  = var.app_name
-  location              = "east us"
-  resource_group_name   = "NageswarRG"
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  https_only            = true
-  site_config { 
-    minimum_tls_version = "1.2"
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
 
+resource "azurerm_app_service" "example" {
+  name                = var.app_name
+  location            = "east us"
+  resource_group_name = "NageswarRG"
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
 
 resource "azurerm_storage_account" "storage" {
   name                     = "stor${random_string.random.id}"
